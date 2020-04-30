ede3744ea745726eb599ade99ac1de1b2cf6c13e
Chris <raoxc@mogulinker.com>
2020-04-30 16:06:36 +0800

    cssmodules：设为false，试用默认规则，xx.module.less
    cssmodules：设为false，试用默认规则，xx.module.less

--- .fatherrc.js
+++ .fatherrc.js
@@ -2,7 +2,7 @@
  * @Author: Chris
  * @Date: 2020-04-28 11:37:08
  * @LastEditors: Chris
- * @LastEditTime: 2020-04-29 19:06:35
+ * @LastEditTime: 2020-04-30 16:00:49
  * @Descripttion: **
  */
 import commonjs from 'rollup-plugin-commonjs';
@@ -13,27 +13,24 @@ export default {
   doc: {
     base: '/umi_cps',
   },
-  cssModules: true,
-  cssModules: {
-    generateScopedName: 'mglinker_[name]__[local]___[hash:base64:5]',
-  },
-  // namedExports:{
-  // 'react': ['createElement', 'Component' ],
-  // 'node_modules/react/cjs/react.development.js': ['createElement', 'Component'],
-  // }，
+  cssModules: false,
+  // cssModules: {
+  //   generateScopedName: 'mglinker_[name]__[local]___[hash:base64:5]',
+  // },
+  // extractCSS: true,
   extraRollupPlugins: [
     commonjs({
       // include: 'node_modules/**',
       // namedExports: { 'react': ['createElement', 'Component', 'Fragment', 'createContext', 'useContext', 'useEffect', 'useMemo', 'useRef', 'useState', 'forwardRef', 'Suspense', 'useCallback', 'memo' ] },
       namedExports: { 'react': ['createElement', 'Component'] },
     })
-  ]
+  ],
+  extraBabelPlugins: [
+    ['babel-plugin-import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    }],
+  ],
   // cjs: 'rollup',
-  // extraBabelPlugins:[
-  //   ['babel-plugin-import', {
-  //     libraryName: 'antd',
-  //     libraryDirectory: 'es',
-  //     style: true,
-  //   }],
-  // ],
 }
